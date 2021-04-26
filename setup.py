ada434923a00385a9dd0a906d8907e1d7bb05fff
Jorge Arevalo <jorgeas80@tuta.io>
2021-04-27 01:22:20 +0200

    Delete unnecessary stuff and improve setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,33 @@
 from setuptools import setup
 
+requirements = [
+    'requests',
+    'm3u8',
+    'ffmpeg-python',
+]
+
 setup(
-  name="twitter-video-downloader",
-  version="0.0.1",
-  scripts=["twitter_video_downloader"]
+    name='twitter-video-downloader',
+    version='0.1.0',
+    description='Library for Twitter video downloading',
+    author='Jorge Arévalo (based on the original scripy by h4ckninja)',
+    author_email='jorgeas80@tuta.io',
+    license='MIT',
+    url='https://github.com/jorgeas80/twitter-video-downloader',
+    keywords=['twitter', 'video', 'download'],
+    install_requires=requirements,
+    extras_require={
+        'dev': [
+            'ipython',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Topic :: Multimedia :: Video',
+        'Topic :: Multimedia :: Video :: Conversion',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ]
 )
